d30be7098efc6ed886677d4b54c98f264f4966c0
Vadim Colcev <vadimkolchev@gmail.com>
2020-07-19 13:25:03 +0300

    Remove unnecessary console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('client-side JS is loaded');
-
 const weatherForm = document.querySelector('form');
 const searchElement = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
